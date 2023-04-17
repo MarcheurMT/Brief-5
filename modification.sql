9bc48e8670e27f599329e93443cc2a7c6e6cdeec
Estaque Rémi <103458575+MarcheurMT@users.noreply.github.com>
2023-04-17 23:44:10 +0200

    modification final

--- modification.sql
+++ modification.sql
@@ -1,9 +1,8 @@
--- modifier une donnée dans un tableau, ici passer le prix du VIP avec id 13 Ã  150 au lieux de 200 : 
+-- modifier une donnée dans un tableau, ici passer le prix du VIP avec id 13 à 150 au lieux de 200 : 
 
 UPDATE `statut` SET `prix` = '150' WHERE `statut`.`id_statut` = 13;
 
 --exemple d'alteration
 
 ALTER TABLE assurances 
-ADD COLUMN mail_assurance varchar, 
-ADD CONSTRAINT `fk_id_assurance` FOREIGN KEY (id_assurance) REFERENCES assurance (id_assurance ON DELETE SET NULL ON UPDATE CASCADE);
+  ADD CONSTRAINT `fk_assurance_id_adresse` FOREIGN KEY (`id_adresse`) REFERENCES `adresse` (`id_adresse`) ON DELETE SET NULL ON UPDATE CASCADE
