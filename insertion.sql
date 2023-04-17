c16a9299f4e2bf598e2accbdffc97f84d658a03b
Estaque Rémi <103458575+MarcheurMT@users.noreply.github.com>
2023-04-17 23:43:41 +0200

    insertion final

--- insertion.sql
+++ insertion.sql
@@ -1,15 +1,15 @@
 INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'VIP', '200', '12');
 
 --ici on insert dans le tableau statut une nouvelle ligne, qui sera composer d'un id, du nom du statut donc "statut", de son "prix" et du nombre de mois effectif donc "n_mois". 
---On dÃ©termine ensuite (ou pas si prÃ©dÃ©terminer) les values dans le mÃªme ordre des donnÃ©es inscrite. Puis on fait la mÃªme chose pour le reste de la table :
+--On détermine ensuite (ou pas si prédéterminer) les values dans le même ordre des données inscrite. Puis on fait la même chose pour le reste de la table :
 
 INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'premium version international', '175', '12');
 INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'premuim', '150', '12');
 INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'cycliste international', '150', '12');
-INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'cycliste d'eau douce', '100', '12');
+INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, "cycliste d'eau douce", '100', '12');
 INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'le muscle est partout', '150', '12');
 INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'brain-i fit international', '130', '12');
 INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'b less-fit', '125', '12');
 INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'membre', '100', '12');
 INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'coach', '50', '12');
-INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'agent de nettoyage et de sÃ©curitÃ©', 'NULL', 'NULL');
+INSERT INTO `statut` (`id_statut`, `statut`, `prix`, `n_mois`) VALUES (NULL, 'agent de nettoyage et de sécurité', 'NULL', 'NULL');
